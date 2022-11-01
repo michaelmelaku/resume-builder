7592a60cb3beffd3d5fecf468999190f35bfd855
Michael Melaku <mmelaku9@gmail.com>
2022-11-01 18:34:26 +0300

    collapsible

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -2,13 +2,15 @@ import React, { useRef, useState } from "react";
 import ReactToPrint from "react-to-print";
 import {   Mail, Database, Download, Save } from "react-feather";
 
+import Collapsible from 'react-collapsible';
+
 import Editor from "../Editor/Editor";
 import Resume from "../Resume/Resume";
 
 import styles from "./Body.module.css";
 
 function Body() {
-  const colors = ["#239ce2", "#48bb78", "#0bc5ea", "#a0aec0", "#ed8936"];
+  const colors = ["#239ce2", "#48bb78", "#0bc5ea", "#a0aec0", "#ed8936", "#ed1111"];
   const sections = {
     basicInfo: "Basic Info",
     workExp: "Work Experience",
@@ -63,6 +65,7 @@ function Body() {
     <div className={styles.container}>
       <p className={styles.heading}>Resume Builder</p>
       <div className={styles.toolbar}>
+        <Collapsible trigger={<button>Colors</button>}>
         <div className={styles.colors}>
           {colors.map((item) => (
             <span
@@ -75,6 +78,7 @@ function Body() {
             />
           ))}
         </div>
+        </Collapsible>
         <ReactToPrint
           trigger={() => {
             return (
